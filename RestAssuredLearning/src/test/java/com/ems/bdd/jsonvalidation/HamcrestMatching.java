fba2283655a72a96f69ea1ece0653df856493b38
Ajay <ajaychandru20@gmail.com>
2025-03-01 11:37:29 +0530

    started json path

---
+++ RestAssuredLearning/src/test/java/com/ems/bdd/jsonvalidation/HamcrestMatching.java
@@ -0,0 +1,34 @@
+package com.ems.bdd.jsonvalidation;
+
+import io.restassured.module.jsv.JsonSchemaValidator;
+import org.apache.commons.io.FileUtils;
+import org.hamcrest.MatcherAssert;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class HamcrestMatching {
+
+    @Test
+    public void validateDataUsingHamcrest() throws IOException {
+
+        File jsonInput = new File("src/test/resources/jsonValidation/response.json");
+
+
+         String stringJsonInput = FileUtils.readFileToString(jsonInput,"UTF-8");
+
+        InputStream readResponseSchema = new FileInputStream("src/test/resources/jsonValidation/schema.json");
+
+        MatcherAssert.assertThat(stringJsonInput, JsonSchemaValidator.matchesJsonSchema(readResponseSchema));
+
+
+
+
+
+    }
+
+
+}
--- RestAssuredLearning/src/test/java/com/ems/bdd/jsonvalidation/ValidateUsingNetworkNT.java
+++ RestAssuredLearning/src/test/java/com/ems/bdd/jsonvalidation/ValidateUsingNetworkNT.java
@@ -1,10 +1,44 @@
 package com.ems.bdd.jsonvalidation;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.networknt.schema.JsonSchema;
+import com.networknt.schema.JsonSchemaFactory;
+import com.networknt.schema.SpecVersion;
+import com.networknt.schema.ValidationMessage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Set;
+
 public class ValidateUsingNetworkNT {
 
-    public void validateJsonSchemaUsingNetworkNt(){
+    @Test
+    public void validateJsonSchemaUsingNetworkNt() throws IOException {
+        // Load JSON file
+        File inputJson = new File("src/test/resources/jsonValidation/response.json");
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode jsonNodeProperties = mapper.readTree(inputJson);
+        System.out.println("JSON Response: " + jsonNodeProperties);
 
+        // Load JSON Schema
+        InputStream responseSchema = new FileInputStream("src/test/resources/jsonValidation/schema.json");
+        JsonSchemaFactory jsonSchemaFactory = JsonSchemaFactory.getInstance(SpecVersion.VersionFlag.V4);
+        JsonSchema schema = jsonSchemaFactory.getSchema(responseSchema);
+
+        // Validate JSON against Schema
+        Set<ValidationMessage> validate = schema.validate(jsonNodeProperties);
+
+        // Print validation errors, if any
+        for (ValidationMessage message : validate) {
+            System.out.println("Validation Error: " + message);
+        }
+
+        // Assert that the validation passed
+        Assert.assertTrue(validate.isEmpty(), "JSON does not match the schema!");
     }
-
-
 }
